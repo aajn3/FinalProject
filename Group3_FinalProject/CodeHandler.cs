1eb3d381effd86823d546720e01037c29ebde524
aagavin <aagavin@gmail.com>
2016-08-04 17:10:52 -0400

    switched code the correct class

--- Group3_FinalProject/CodeHandler.cs
+++ Group3_FinalProject/CodeHandler.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
+
 
 
 namespace Group3_FinalProject
@@ -10,40 +9,10 @@ namespace Group3_FinalProject
 
     class CodeHandler
     {
-        Dictionary<char, int> Occurences; //
-        Dictionary<string, string> Frequency;
-        Dictionary<string, string> Ordered_Frequency;
-        
-
         CodeHandler()
         {
-            Occurences = new Dictionary<char, int>();
-            Frequency = new Dictionary<string, string>();
-            Ordered_Frequency = new Dictionary<string, string>();
-
-            // add keys to occurences
-            string keys = "abcdefghijklmnopqrstuvwxyz";
             
-            foreach(char key in keys){
-                Occurences.Add(key,0);
-            }
         }
-
-        public void CountOccorences(string path){
-            using(StreamReader reader = new StreamReader(path)){
-                string line;//line of file
-
-                while ((line = reader.ReadLine()) != null)
-                {
-                    // read each char
-                    foreach (char c in line)
-                    {
-                        Occurences[c] = Occurences[c]++;
-                    }
-                }
-            }
-            Console.WriteLine(Occurences);
-        }//CountOccorences
     }
 
 
--- Group3_FinalProject/TextHandler.cs
+++ Group3_FinalProject/TextHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,40 @@ namespace Group3_FinalProject
 {
     class TextHandler
     {
+        Dictionary<char, int> Occurences; //
+        Dictionary<string, string> Frequency;
+        Dictionary<string, string> Ordered_Frequency;
+        
+
+        TextHandler()
+        {
+            Occurences = new Dictionary<char, int>();
+            Frequency = new Dictionary<string, string>();
+            Ordered_Frequency = new Dictionary<string, string>();
+
+            // add keys to occurences
+            string keys = "abcdefghijklmnopqrstuvwxyz";
+            
+            foreach(char key in keys){
+                Occurences.Add(key,0);
+            }
+        }
+
+        public void CountOccorences(string path){
+            using(StreamReader reader = new StreamReader(path)){
+                string line;//line of file
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // read each char
+                    foreach (char c in line)
+                    {
+                        Occurences[c] = Occurences[c]++;
+                    }
+                }
+            }
+            Console.WriteLine(Occurences);
+        }//CountOccorences
     }
 
 }
