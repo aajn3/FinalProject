b1341a6a7fc72dc5db730f199ec4ed718a6223bc
aagavin <aagavin@gmail.com>
2016-08-05 12:19:22 -0400

    Fixed Occr function and added .gitignore

--- Group3_FinalProject/TextHandler.cs
+++ Group3_FinalProject/TextHandler.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace Group3_FinalProject
 {
-    class TextHandler
+    public class TextHandler
     {
         Dictionary<char, int> Occurences; //
         Dictionary<string, string> Frequency;
         Dictionary<string, string> Ordered_Frequency;
         
 
-        TextHandler()
+        public TextHandler()
         {
             Occurences = new Dictionary<char, int>();
             Frequency = new Dictionary<string, string>();
@@ -26,22 +26,40 @@ namespace Group3_FinalProject
             foreach(char key in keys){
                 Occurences.Add(key,0);
             }
+            Occurences.Add(' ', 0);
         }
 
         public void CountOccorences(string path){
+            if (File.Exists(path))
+            {
+                Console.WriteLine("I'm here");
+            }
             using(StreamReader reader = new StreamReader(path)){
                 string line;//line of file
+                char index;
+                int value;
 
                 while ((line = reader.ReadLine()) != null)
                 {
                     // read each char
                     foreach (char c in line)
                     {
-                        Occurences[c] = Occurences[c]++;
+                        index = Char.ToLower(c);
+
+                        if ("abcdefghijklmnopqrstuvwxyz".Contains(index))
+                        {
+                            value=(Occurences[index])+1;
+                            Occurences[index] = value;
+                        }
+                        else
+                        {
+                            value = (Occurences[' ']) + 1;
+                            Occurences[' '] = value;
+                        }
                     }
                 }
             }
-            Console.WriteLine(Occurences);
+
         }//CountOccorences
     }
 
